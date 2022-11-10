ad972e49a4ab229cacdfaa9e0955d0a7097aafaf
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-10 11:33:44 +0900

    2. findById(): a. Controller

--- src/main/java/com/mustache/bbs3/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs3/controller/ArticleController.java
@@ -5,9 +5,13 @@ import com.mustache.bbs3.domain.entity.Article;
 import com.mustache.bbs3.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/articles")
@@ -29,5 +33,17 @@ public class ArticleController {
     public String createArticle(ArticleDto articleDto) {
         Article savedArticle = articleRepository.save(articleDto.toEntity());
         return String.format("id: %d", savedArticle.getId());
+    }
+
+    @GetMapping("/{id}")
+    public String showOne(@PathVariable Long id, Model model) {
+        Optional<Article> optionalArticle = articleRepository.findById(id);
+
+        if (optionalArticle.isPresent()) {
+            model.addAttribute("article", optionalArticle.get());
+            return "/show";
+        } else {
+            return "/error";
+        }
     }
 }
